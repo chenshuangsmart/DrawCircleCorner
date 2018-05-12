7b1408127672e6c51e882dce1776395fbbc0a2b7
陈双 <chenshuang@banggood.com>
2018-05-12 09:43:54 +0800

    draw circle corner
    绘制指定方向的圆角

--- 0512_drawCircleCorner/ViewController.m
+++ 0512_drawCircleCorner/ViewController.m
@@ -17,8 +17,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self drawLeftTopCircleCorner];
+    [self drawRightTopCircleCorner];
+    [self drawTopCircleCorner];
 }
 
+- (void)drawLeftTopCircleCorner {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view.center = CGPointMake(self.view.bounds.size.width * 0.25, self.view.bounds.size.height * 0.25);
+    view.backgroundColor = [UIColor redColor];
+    view.layer.masksToBounds = YES;
+    
+    //这里设置需要绘制的圆角
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:view.bounds
+                                                   byRoundingCorners:UIRectCornerTopLeft//左上角
+                                                         cornerRadii:CGSizeMake(25, 25)];//设置圆角大小，弧度为3
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = view.bounds;
+    maskLayer.path = maskPath.CGPath;
+    view.layer.mask = maskLayer;
+    [self.view addSubview:view];
+}
+
+- (void)drawRightTopCircleCorner {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view.center = CGPointMake(self.view.bounds.size.width * 0.75, self.view.bounds.size.height * 0.25);
+    view.backgroundColor = [UIColor redColor];
+    view.layer.masksToBounds = YES;
+    
+    //这里设置需要绘制的圆角
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:view.bounds
+                                                   byRoundingCorners:UIRectCornerTopRight//右上角
+                                                         cornerRadii:CGSizeMake(25, 25)];//设置圆角大小，弧度为3
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = view.bounds;
+    maskLayer.path = maskPath.CGPath;
+    view.layer.mask = maskLayer;
+    [self.view addSubview:view];
+}
+
+- (void)drawTopCircleCorner {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view.center = CGPointMake(self.view.bounds.size.width * 0.25, self.view.bounds.size.height * 0.75);
+    view.backgroundColor = [UIColor redColor];
+    view.layer.masksToBounds = YES;
+    
+    //这里设置需要绘制的圆角
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:view.bounds
+                                                   byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight//
+                                                         cornerRadii:CGSizeMake(25, 25)];//设置圆角大小，弧度为3
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = view.bounds;
+    maskLayer.path = maskPath.CGPath;
+    view.layer.mask = maskLayer;
+    [self.view addSubview:view];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
